23d8e54d73af153ef99ec72ff210dcfd83af90e7
aleksandrakaszuba <ola.kaszuba@gmail.com>
2018-02-08 22:35:08 -0500

    Removed unnecessary file

---
+++ cmake-build-debug/CMakeFiles/Guesser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Guesser.dir/src/guesser.c.o"
+  "Guesser.pdb"
+  "Guesser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Guesser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Guesser.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Guesser.
+# This may be replaced when dependencies are built.
